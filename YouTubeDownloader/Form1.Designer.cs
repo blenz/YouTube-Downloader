4c53165e99ef18b196d7121ad192a071efb9494e
Brett <bbluegga@yahoo.com>
2015-12-31 15:33:33 -0800

    Initial commit

---
+++ YouTubeDownloader/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace YouTubeDownloader
+{
+    partial class YouTubeDLForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YouTubeDLForm));
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.lblDownloadPath = new System.Windows.Forms.Label();
+            this.txtDownloadPath = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.chkOpenAfterDownload = new System.Windows.Forms.CheckBox();
+            this.lblYouTubeURL = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.Image = ((System.Drawing.Image)(resources.GetObject("picBox.Image")));
+            this.picBox.Location = new System.Drawing.Point(275, 12);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(132, 73);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // lblDownloadPath
+            // 
+            this.lblDownloadPath.AutoSize = true;
+            this.lblDownloadPath.Location = new System.Drawing.Point(25, 94);
+            this.lblDownloadPath.Name = "lblDownloadPath";
+            this.lblDownloadPath.Size = new System.Drawing.Size(83, 13);
+            this.lblDownloadPath.TabIndex = 1;
+            this.lblDownloadPath.Text = "Download Path:";
+            // 
+            // txtDownloadPath
+            // 
+            this.txtDownloadPath.Location = new System.Drawing.Point(114, 91);
+            this.txtDownloadPath.Name = "txtDownloadPath";
+            this.txtDownloadPath.Size = new System.Drawing.Size(327, 20);
+            this.txtDownloadPath.TabIndex = 2;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(447, 89);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Choose";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            // 
+            // chkOpenAfterDownload
+            // 
+            this.chkOpenAfterDownload.AutoSize = true;
+            this.chkOpenAfterDownload.Location = new System.Drawing.Point(528, 93);
+            this.chkOpenAfterDownload.Name = "chkOpenAfterDownload";
+            this.chkOpenAfterDownload.Size = new System.Drawing.Size(128, 17);
+            this.chkOpenAfterDownload.TabIndex = 4;
+            this.chkOpenAfterDownload.Text = "Open After Download";
+            this.chkOpenAfterDownload.UseVisualStyleBackColor = true;
+            // 
+            // lblYouTubeURL
+            // 
+            this.lblYouTubeURL.AutoSize = true;
+            this.lblYouTubeURL.Location = new System.Drawing.Point(28, 131);
+            this.lblYouTubeURL.Name = "lblYouTubeURL";
+            this.lblYouTubeURL.Size = new System.Drawing.Size(79, 13);
+            this.lblYouTubeURL.TabIndex = 5;
+            this.lblYouTubeURL.Text = "YouTube URL:";
+            // 
+            // YouTubeDLForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 261);
+            this.Controls.Add(this.lblYouTubeURL);
+            this.Controls.Add(this.chkOpenAfterDownload);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtDownloadPath);
+            this.Controls.Add(this.lblDownloadPath);
+            this.Controls.Add(this.picBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "YouTubeDLForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "YouTube Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Label lblDownloadPath;
+        private System.Windows.Forms.TextBox txtDownloadPath;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.CheckBox chkOpenAfterDownload;
+        private System.Windows.Forms.Label lblYouTubeURL;
+    }
+}
+

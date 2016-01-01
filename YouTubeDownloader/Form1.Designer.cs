9c8b9ef481cec07fb6d5eec1571b84acbbdec0e1
Brett <bbluegga@yahoo.com>
2015-12-31 16:16:48 -0800

    Aligned UI

--- YouTubeDownloader/Form1.Designer.cs
+++ YouTubeDownloader/Form1.Designer.cs
@@ -50,9 +50,9 @@
             this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.picBox.Image = ((System.Drawing.Image)(resources.GetObject("picBox.Image")));
-            this.picBox.Location = new System.Drawing.Point(275, 12);
+            this.picBox.Location = new System.Drawing.Point(271, 33);
             this.picBox.Name = "picBox";
-            this.picBox.Size = new System.Drawing.Size(132, 73);
+            this.picBox.Size = new System.Drawing.Size(151, 73);
             this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBox.TabIndex = 0;
             this.picBox.TabStop = false;
@@ -60,7 +60,7 @@
             // lblDownloadPath
             // 
             this.lblDownloadPath.AutoSize = true;
-            this.lblDownloadPath.Location = new System.Drawing.Point(25, 94);
+            this.lblDownloadPath.Location = new System.Drawing.Point(18, 116);
             this.lblDownloadPath.Name = "lblDownloadPath";
             this.lblDownloadPath.Size = new System.Drawing.Size(83, 13);
             this.lblDownloadPath.TabIndex = 1;
@@ -68,14 +68,14 @@
             // 
             // txtDownloadPath
             // 
-            this.txtDownloadPath.Location = new System.Drawing.Point(114, 91);
+            this.txtDownloadPath.Location = new System.Drawing.Point(126, 116);
             this.txtDownloadPath.Name = "txtDownloadPath";
             this.txtDownloadPath.Size = new System.Drawing.Size(327, 20);
             this.txtDownloadPath.TabIndex = 2;
             // 
             // btnChoose
             // 
-            this.btnChoose.Location = new System.Drawing.Point(447, 89);
+            this.btnChoose.Location = new System.Drawing.Point(459, 116);
             this.btnChoose.Name = "btnChoose";
             this.btnChoose.Size = new System.Drawing.Size(75, 23);
             this.btnChoose.TabIndex = 3;
@@ -85,7 +85,7 @@
             // chkOpenAfterDownload
             // 
             this.chkOpenAfterDownload.AutoSize = true;
-            this.chkOpenAfterDownload.Location = new System.Drawing.Point(528, 93);
+            this.chkOpenAfterDownload.Location = new System.Drawing.Point(547, 116);
             this.chkOpenAfterDownload.Name = "chkOpenAfterDownload";
             this.chkOpenAfterDownload.Size = new System.Drawing.Size(128, 17);
             this.chkOpenAfterDownload.TabIndex = 4;
@@ -95,7 +95,7 @@
             // lblYouTubeURL
             // 
             this.lblYouTubeURL.AutoSize = true;
-            this.lblYouTubeURL.Location = new System.Drawing.Point(25, 131);
+            this.lblYouTubeURL.Location = new System.Drawing.Point(18, 147);
             this.lblYouTubeURL.Name = "lblYouTubeURL";
             this.lblYouTubeURL.Size = new System.Drawing.Size(79, 13);
             this.lblYouTubeURL.TabIndex = 5;
@@ -103,7 +103,7 @@
             // 
             // txtYouTubeURL
             // 
-            this.txtYouTubeURL.Location = new System.Drawing.Point(114, 128);
+            this.txtYouTubeURL.Location = new System.Drawing.Point(126, 147);
             this.txtYouTubeURL.Name = "txtYouTubeURL";
             this.txtYouTubeURL.Size = new System.Drawing.Size(327, 20);
             this.txtYouTubeURL.TabIndex = 6;
@@ -111,14 +111,14 @@
             // comboBoxFileType
             // 
             this.comboBoxFileType.FormattingEnabled = true;
-            this.comboBoxFileType.Location = new System.Drawing.Point(448, 127);
+            this.comboBoxFileType.Location = new System.Drawing.Point(459, 147);
             this.comboBoxFileType.Name = "comboBoxFileType";
             this.comboBoxFileType.Size = new System.Drawing.Size(74, 21);
             this.comboBoxFileType.TabIndex = 7;
             // 
             // btnDownload
             // 
-            this.btnDownload.Location = new System.Drawing.Point(554, 126);
+            this.btnDownload.Location = new System.Drawing.Point(547, 147);
             this.btnDownload.Name = "btnDownload";
             this.btnDownload.Size = new System.Drawing.Size(75, 23);
             this.btnDownload.TabIndex = 8;
@@ -128,7 +128,7 @@
             // lblCurrentFileStatus
             // 
             this.lblCurrentFileStatus.AutoSize = true;
-            this.lblCurrentFileStatus.Location = new System.Drawing.Point(25, 161);
+            this.lblCurrentFileStatus.Location = new System.Drawing.Point(18, 182);
             this.lblCurrentFileStatus.Name = "lblCurrentFileStatus";
             this.lblCurrentFileStatus.Size = new System.Drawing.Size(91, 13);
             this.lblCurrentFileStatus.TabIndex = 9;
@@ -137,7 +137,7 @@
             // lblFileDownloading
             // 
             this.lblFileDownloading.AutoSize = true;
-            this.lblFileDownloading.Location = new System.Drawing.Point(122, 161);
+            this.lblFileDownloading.Location = new System.Drawing.Point(115, 182);
             this.lblFileDownloading.Name = "lblFileDownloading";
             this.lblFileDownloading.Size = new System.Drawing.Size(35, 13);
             this.lblFileDownloading.TabIndex = 10;
@@ -146,7 +146,7 @@
             // lblProgressBar
             // 
             this.lblProgressBar.AutoSize = true;
-            this.lblProgressBar.Location = new System.Drawing.Point(25, 194);
+            this.lblProgressBar.Location = new System.Drawing.Point(18, 215);
             this.lblProgressBar.Name = "lblProgressBar";
             this.lblProgressBar.Size = new System.Drawing.Size(102, 13);
             this.lblProgressBar.TabIndex = 11;
@@ -154,7 +154,7 @@
             // 
             // pgDownload
             // 
-            this.pgDownload.Location = new System.Drawing.Point(133, 194);
+            this.pgDownload.Location = new System.Drawing.Point(126, 215);
             this.pgDownload.Name = "pgDownload";
             this.pgDownload.Size = new System.Drawing.Size(389, 13);
             this.pgDownload.TabIndex = 12;
@@ -163,7 +163,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 261);
+            this.ClientSize = new System.Drawing.Size(693, 261);
             this.Controls.Add(this.pgDownload);
             this.Controls.Add(this.lblProgressBar);
             this.Controls.Add(this.lblFileDownloading);
